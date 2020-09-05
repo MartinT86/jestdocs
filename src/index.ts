2299ad43ef52cc83b5b52f6c070827583eecb0b8
Martin Tierney <martin.tierney86@hotmail.co.uk>
2020-09-05 17:24:33 +0100

    added babel parser for ts support

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-import { parse } from "acorn";
+// import { parse } from "acorn";
 import  { promises } from 'fs'
+import {parse} from '@babel/parser'
 
 type DocResult = string[] 
 
 const example = async (): Promise<DocResult> => {
-    //need to read the file as a string
     const fileData = await promises.readFile('./test/two.test.ts')
-  const parsed =  parse(fileData.toString(), { ecmaVersion: 2020 });
-  return ['parsed']
+
+    const parsed = parse(fileData.toString())
+  // const parsed =  parse(fileData.toString(), { ecmaVersion: 2020 });
+  
+  return ['Name of the group example']
 };
 
 export { example };
